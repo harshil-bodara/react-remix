91554cc1ddd04365886aedc3f9aec364be4e9959
harshil-bodara <developer.harshilbodara@gmail.com>
2022-10-18 21:12:52 +0530

    create a loader hooks

--- app/routes/posts/$postid.tsx
+++ app/routes/posts/$postid.tsx
@@ -1,9 +1,26 @@
-import { useParams } from '@remix-run/react'
+import { useParams, useLoaderData } from '@remix-run/react'
+
+export const loader = () => {
+    const data = {
+        posts: [
+            { id: 1, name: "post 1" },
+            { id: 2, name: "post 2" },
+            { id: 3, name: "post 3" }
+        ]
+    }
+    return data
+}
 
 const Posts = () => {
     const { postid } = useParams();
+    const { posts } = useLoaderData();
     return (
-        <div>Post ID: {postid}</div>
+        <>
+            <div>Post ID: {postid}</div>
+            {posts.map((val: any) => {
+                return <h3>{val.name}</h3>
+            })}
+        </>
     )
 }
 
